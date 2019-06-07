b21f42423762ade67c57444fbab9e794542438a0
Jas <c.jasminher@gmail.com>
2019-06-07 15:59:46 -0400

    Updates

---
+++ precache-manifest.be54434e3197078c45ec0959a6aefd72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49bf62d421c95b5d3fc9f3ae06a65e19",
+    "url": "/contact-cards/index.html"
+  },
+  {
+    "revision": "ab72f4c65e12477e5c44",
+    "url": "/contact-cards/static/css/main.45bceaf5.chunk.css"
+  },
+  {
+    "revision": "21e31637f676d46e7c7e",
+    "url": "/contact-cards/static/js/2.b9b46daa.chunk.js"
+  },
+  {
+    "revision": "ab72f4c65e12477e5c44",
+    "url": "/contact-cards/static/js/main.59712069.chunk.js"
+  },
+  {
+    "revision": "0e3a504bb650297ae782",
+    "url": "/contact-cards/static/js/runtime~main.eb3469bd.js"
+  }
+]);
